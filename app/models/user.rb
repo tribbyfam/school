4069cfc310094e2653778676807bf833d286fd9b
Mac <mac@MacBook-Pro.fios-router.home>
2018-09-30 19:46:57 -0500

    added first and last name validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_presence_of :first_name, :last_name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user = User.create(email: "test@test.com", password: "123123", password_confirmation: "123123", first_name: "John", last_name: "Snow")
+    end
+    it "can be created" do  
+      expect(@user).to be_valid
+    end
+    it "cannot be created without first name and last name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
